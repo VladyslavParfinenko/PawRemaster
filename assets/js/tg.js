74ab4432cf3ee1889b4e7b8742f30e7bd60460f7
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-11-07 17:17:04 +0200

    tg bot add

---
+++ assets/js/tg.js
@@ -0,0 +1,18 @@
+const TOKEN = "6639837469:AAEGvcOk7hVSMTZ0HFjgNM2YtMHqv2uCr04";
+const CHAT_ID="-1002019195458";
+const URL_API=`https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+document.getElementById("tg").addEventListener('submit',function(e){
+    e.preventDefault();
+    
+    let message = `<b>Заявка с сайта PawBags</b>\n`;
+    message +=`<b>Відправник: </b> ${this.name.value }\n`;
+    message +=`<b>Телефон: </b> ${this.tel.value }\n`;
+    message +=`<b>Коментар: </b> ${this.comment.value }`;
+    
+    axios.post(URL_API,{
+        chat_id:CHAT_ID,
+        parse_mode:'html',
+        text:message
+    });
+})
